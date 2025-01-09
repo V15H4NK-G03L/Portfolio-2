586645eedb0d349ff8c3f9fdf1b6b1904d2c94af
Vishank Goel <vishankgoel11@gmail.com>
2025-01-09 22:18:54 +0530

    Integrated Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from 'next/font/google';
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans"});
 const calistoga = Calistoga({ subsets: ['latin'], variable: "--font-serif", weight:["400"]});
@@ -26,6 +27,7 @@ export default function RootLayout({
         )}
         >
           {children}
+          <SpeedInsights />
         </body>
     </html>
   );
